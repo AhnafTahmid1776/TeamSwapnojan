2fdc7c177968a0b38d7f62b0d0832dbfc31f2fbb
ahnaf Tahmid Saad <86840261+AhnafTahmid1776@users.noreply.github.com>
2022-02-06 23:37:12 +0600

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,1 +1,59 @@
+const toggle = document.getElementById('toggle')
+const nav = document.getElementById('nav')
+toggle.addEventListener('click', () => nav.classList.toggle('active'))
+
+
+
+
+
+
+
+const panels = document.querySelectorAll('.panel')
+panels.forEach((panel) => {
+    panel.addEventListener('click',() => {
+        removeActiveClasses()
+        panel.classList.add('activ')
+    })
+
+})
+
+function removeActiveClasses(){
+
+    panels.forEach(panel => {
+     panel.classList.remove('activ')
+    })
+}
+
+
+
+
+const books = document.querySelectorAll('.book')
+
+window.addEventListener('scroll', checkBoxes)
+
+
+checkBoxes();
+
+function checkBoxes(){
+    const triggerBottom = (window.innerHeight/5) * 4
+
+    // dividing the innerheight by 5 parts , taking 4 part as bottom line
+books.forEach(book => {
+    const bookTop = book.getBoundingClientRect().top
+    //describe a size and position of rectangle relative to screen
+
+    if(bookTop < triggerBottom){
+        book.classList.add('show')
+
+    }
+    else{
+        book.classList.remove('show')
+    }
+})
+
+}
+
+
+
+
 
